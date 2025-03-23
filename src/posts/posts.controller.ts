addea7f9591f1d81e7d94d651f0806ae6ce41b54
chhezue <chhezue@gmail.com>
2025-03-23 15:18:18 +0900

    section 7: Service

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -2,57 +2,12 @@ import {
   Body,
   Controller,
   Get,
-  NotFoundException,
   Param,
   Post,
   Patch,
   Delete,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
-
-/**
- * author: string;
- * title: string;
- * content: string;
- * likeCount: number;
- * commentCount: number;
- */
-
-interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'newjeans_official',
-    title: '뉴진스 민지',
-    content: '메이크업 고치고 있는 민지',
-    likeCount: 100,
-    commentCount: 99,
-  },
-  {
-    id: 2,
-    author: 'newjeans_official',
-    title: '뉴진스 해린',
-    content: '노래 연습 하고 있는 해린',
-    likeCount: 100,
-    commentCount: 99,
-  },
-  {
-    id: 3,
-    author: 'blackpink_official',
-    title: '블랙핑크 로제',
-    content: '종합운동장에서 공연하고 있는 로제',
-    likeCount: 100,
-    commentCount: 99,
-  },
-];
 
 @Controller('posts')
 export class PostsController {
@@ -61,20 +16,13 @@ export class PostsController {
   // GET /posts
   @Get()
   getPosts() {
-    return posts;
+    return this.postsService.getAllPosts();
   }
 
   // GET /posts/:id
   @Get(':id')
   getPost(@Param() id: string) {
-    // +id: string으로 받은 id를 숫자형으로 변환
-    const post = posts.find((post) => post.id === +id);
-
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    return post;
+    return this.postsService.getPostById(+id);
   }
 
   // POST /posts
@@ -85,18 +33,7 @@ export class PostsController {
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
-    const post: PostModel = {
-      id: posts[posts.length - 1].id + 1,
-      author: author,
-      title: title,
-      content: content,
-      likeCount: 0,
-      commentCount: 0,
-    };
-
-    posts = [...posts, post];
-
-    return post;
+    return this.postsService.createPost(author, title, content);
   }
 
   // PATCH /posts/:id
@@ -108,38 +45,12 @@ export class PostsController {
     @Body('title') title?: string,
     @Body('content') content?: string,
   ) {
-    const post = posts.find((post) => post.id === +id);
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    // 존재하는 값만 수정
-    if (author) {
-      post.author = author;
-    }
-    if (title) {
-      post.title = title;
-    }
-    if (content) {
-      post.content = content;
-    }
-
-    posts = posts.map((prevPost) => (prevPost.id === +id ? post : prevPost));
-    return post;
+    return this.postsService.updatePost(+id, author, title, content);
   }
 
   // Delete /posts/:id
   @Delete(':id')
   deletePost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    // 조건을 만족하는 요소만 걸러서 새로운 배열 생성
-    // 삭제할 포스트가 아닌 것만 걸러서 새로운 배열 생성
-    posts = posts.filter((post) => post.id !== +id);
-    return id;
+    return this.postsService.deletePost(+id);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,117 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+/**
+ * author: string;
+ * title: string;
+ * content: string;
+ * likeCount: number;
+ * commentCount: number;
+ */
+
+export interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 100,
+    commentCount: 99,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 해린',
+    content: '노래 연습 하고 있는 해린',
+    likeCount: 100,
+    commentCount: 99,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙핑크 로제',
+    content: '종합운동장에서 공연하고 있는 로제',
+    likeCount: 100,
+    commentCount: 99,
+  },
+];
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  getAllPosts() {
+    return posts;
+  }
+
+  getPostById(id: number) {
+    // +id: string으로 받은 id를 숫자형으로 변환
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    return post;
+  }
+
+  createPost(author: string, title: string, content: string) {
+    const post: PostModel = {
+      id: posts[posts.length - 1].id + 1,
+      author: author,
+      title: title,
+      content: content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+
+    posts = [...posts, post];
+
+    return post;
+  }
+
+  updatePost(
+    postId: number,
+    author?: string,
+    title?: string,
+    content?: string,
+  ) {
+    const post = posts.find((post) => post.id === postId);
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    // 존재하는 값만 수정
+    if (author) {
+      post.author = author;
+    }
+    if (title) {
+      post.title = title;
+    }
+    if (content) {
+      post.content = content;
+    }
+
+    posts = posts.map((prevPost) => (prevPost.id === postId ? post : prevPost));
+    return post;
+  }
+
+  deletePost(postId: number) {
+    const post = posts.find((post) => post.id === postId);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    // 조건을 만족하는 요소만 걸러서 새로운 배열 생성
+    // 삭제할 포스트가 아닌 것만 걸러서 새로운 배열 생성
+    posts = posts.filter((post) => post.id !== postId);
+    return postId;
+  }
+}
