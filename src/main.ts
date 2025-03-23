b01dff6dbd11eb4c41e946d18eed2487dcbb8159
chhezue <chhezue@gmail.com>
2025-03-23 16:08:12 +0900

    section 9: SQL & Docker

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // AppModule을 시작으로 Module을 확장해 나간다는 의미
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
